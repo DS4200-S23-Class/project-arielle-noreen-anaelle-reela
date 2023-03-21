7a5de8c19a557b35dc5e7c72a1792a9754628e4c
Arielle <arielle@MacBook-Pro-92.local>
2023-03-21 19:58:12 -0400

    noreen looks

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var categories = []
+
 var data = {
     "Gender": [
       {
@@ -9,7 +9,7 @@ var data = {
         dataPoints: [
           {
             label: "African American",
-            y: 23036,
+            y: 33427.42858,
             drilldown: "african_american"
           }
         ]
@@ -31,7 +31,7 @@ var data = {
   
     "african_american": [
       {
-        click: wildAnimalsDrilldownHandler,
+        click: raceDrillDownHandler,
         cursor: "pointer",
         name: "With Disability",
         type: "column",
@@ -48,7 +48,7 @@ var data = {
         ]
       },
       {
-        click: petAnimalsDrilldownHandler,
+        click: disabilityDrillDownHandler,
         cursor: "pointer",
         name: "Without Disability",
         type: "column",
@@ -64,37 +64,73 @@ var data = {
           { label: "Industrial Engineer", y: 852.6316 }
         ]
       }
-    ]
-  };
-
-  var chart = new CanvasJS.Chart("chartContainer", {
-    title:{
-      text: "Sample Chart"
-    },
-    animationEnabled: true,
-    axisY: {
-      title: "Count"
-    },
-    axisX: {
-      title: "Occupation"
-    },
-    data: data["Gender"],
-    toolTip: {
-      shared: true
-    },
-    legend: {
-      cursor: "pointer",
-      itemclick: function (e) {
-        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-          e.dataSeries.visible = false;
-        } else {
-          e.dataSeries.visible = true;
-        }
-        chart.render();
-      }
-    }
-  });
+    ],
   
+    "african_american_male": [
+      {
+        click: raceDrillDownHandler,
+        cursor: "pointer",
+        name: "With Disability",
+        type: "column",
+        dataPoints: [
+          { label: "Social Scientist", y: 1459.307333 },
+          { label: "Computer and Information Scientist", y: 12757.99656 },
+          { label: "Mathematical Scientist", y: -1 },
+          { label: "Physical Scientist", y: 1321.584639 },
+          { label: "Psychologist", y: -1 },
+          { label: "Computer Engineer", y: 1817.348478 },
+          { label: "Mechanical Engineer", y: 524.3124 },
+          { label: "Civil Engineer", y: 925.1701 },
+          { label: "Industrial Engineer", y: 589.4737 }
+        ]
+      },
+      {
+        click: raceDrillDownHandler,
+        cursor: "pointer",
+        name: "Without Disability",
+        type: "column",
+        dataPoints: [
+          { label: "Social Scientist", y:  13804.25856 },
+          { label: "Computer and Information Scientist", y: 13077.999116 },
+          { label: "Mathematical Scientist", y: -1 },
+          { label: "Physical Scientist", y: 10457.75671 },
+          { label: "Psychologist", y: -1 },
+          { label: "Computer Engineer", y: 17886.53502},
+          { label: "Mechanical Engineer", y: 6796.642},
+          { label: "Civil Engineer", y:  8822.157},
+          { label: "Industrial Engineer", y:3410.526 }
+        ]
+      }]};
+
+      var chart = new CanvasJS.Chart("chartContainer", {
+        title:{
+          text: "Sample Chart"
+        },
+        animationEnabled: true,
+        axisY: {
+          title: "Count",
+        },
+        axisX: {
+          title: "Occupation"
+        },
+        data: data["Gender"],
+        toolTip: {
+          shared: true
+        },
+        legend: {
+          cursor: "pointer",
+          itemclick: function (e) {
+            if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+              e.dataSeries.visible = false;
+            } else {
+              e.dataSeries.visible = true;
+            }
+            chart.render();
+          }
+        }
+      });
+      chart.render();
+
   function genderDrilldownHandler(e) {
     chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
@@ -124,14 +160,14 @@ var data = {
     chart.render();
   }
   
-  function wildAnimalsDrilldownHandler(e) {
+  function raceDrillDownHandler(e) {
     chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
       axisY: {
         title: "Count"
       },
       axisX: {
-        title: "Occupation"
+        title: "Race/Ethnic Groups by Sex "
       },
       data: data[e.dataPoint.drilldown],
       toolTip: {
@@ -142,14 +178,14 @@ var data = {
     chart.render();
   }
   
-  function petAnimalsDrilldownHandler(e) {
+  function disabilityDrillDownHandler(e) {
     chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
       axisY: {
         title: "Count"
       },
       axisX: {
-        title: "Occupation"
+        title: "Occupation by Disability Status"
       },
       data: data[e.dataPoint.drilldown],
       toolTip: {
