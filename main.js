fd9fa3c24dc9d6c57ee7677c7d27d364f76dc07c
Arielle <arielle@MacBook-Pro-92.local>
2023-03-21 15:54:55 -0400

    visualization and some proofreading

--- main.js
+++ main.js
@@ -1,18 +1,162 @@
-//dimensions for each plot
-const FRAME_HEIGHT = 400;
-const FRAME_WIDTH = 400;
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
-const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
-const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
+var categories = []
+var data = {
+    "Gender": [
+      {
+        click: genderDrilldownHandler,
+        cursor: "pointer",
+        name: "Female",
+        type: "column",
+        dataPoints: [
+          {
+            label: "African American",
+            y: 23036,
+            drilldown: "african_american"
+          }
+        ]
+      },
+      {
+        click: genderDrilldownHandler,
+        cursor: "pointer",
+        name: "Male",
+        type: "column",
+        dataPoints: [
+          {
+            label: "African American",
+            y: 37411,
+            drilldown: "african_american"
+          }
+        ]
+      }
+    ],
+  
+    "african_american": [
+      {
+        click: wildAnimalsDrilldownHandler,
+        cursor: "pointer",
+        name: "With Disability",
+        type: "column",
+        dataPoints: [
+          { label: "Social Scientist", y: 1695.731426 },
+          { label: "Computer and Information Scientist", y: 4040.771409 },
+          { label: "Mathematical Scientist", y: -1 },
+          { label: "Physical Scientist", y: 702.8652387 },
+          { label: "Psychologist", y: -1 },
+          { label: "Computer Engineer", y: 211.7777359 },
+          { label: "Mechanical Engineer", y: 47.11213 },
+          { label: "Civil Engineer", y: 217.6871 },
+          { label: "Industrial Engineer", y: 147.3684 }
+        ]
+      },
+      {
+        click: petAnimalsDrilldownHandler,
+        cursor: "pointer",
+        name: "Without Disability",
+        type: "column",
+        dataPoints: [
+          { label: "Social Scientist", y: 16040.70268 },
+          { label: "Computer and Information Scientist", y: 4142.124091 },
+          { label: "Mathematical Scientist", y: -1 },
+          { label: "Physical Scientist", y: 5561.803193 },
+          { label: "Psychologist", y: -1 },
+          { label: "Computer Engineer", y: 2084.338769 },
+          { label: "Mechanical Engineer", y: 610.7128 },
+          { label: "Civil Engineer", y: 2075.802 },
+          { label: "Industrial Engineer", y: 852.6316 }
+        ]
+      }
+    ]
+  };
 
-//append second frame object to page's body
-const FRAME1 = d3.select("#vis1")
-.append("svg")
-.attr("height", FRAME_HEIGHT)
-.attr("width", FRAME_WIDTH)
-.attr("class", "frame");
-
-
-d3.csv('data/pm03_data.csv').then((data) => {
-    console.log(data)
-});
+  var chart = new CanvasJS.Chart("chartContainer", {
+    title:{
+      text: "Sample Chart"
+    },
+    animationEnabled: true,
+    axisY: {
+      title: "Count"
+    },
+    axisX: {
+      title: "Occupation"
+    },
+    data: data["Gender"],
+    toolTip: {
+      shared: true
+    },
+    legend: {
+      cursor: "pointer",
+      itemclick: function (e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+          e.dataSeries.visible = false;
+        } else {
+          e.dataSeries.visible = true;
+        }
+        chart.render();
+      }
+    }
+  });
+  
+  function genderDrilldownHandler(e) {
+    chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      axisY: {
+        title: "Count"
+      },
+      axisX: {
+        title: "Occupation"
+      },
+      data: data[e.dataPoint.drilldown],
+      toolTip: {
+        shared: true
+      },
+      legend: {
+        cursor: "pointer",
+        itemclick: function (e) {
+          if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+          } else {
+            e.dataSeries.visible = true;
+          }
+          chart.render();
+        }
+      }
+    });
+  
+    chart.render();
+  }
+  
+  function wildAnimalsDrilldownHandler(e) {
+    chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      axisY: {
+        title: "Count"
+      },
+      axisX: {
+        title: "Occupation"
+      },
+      data: data[e.dataPoint.drilldown],
+      toolTip: {
+        shared: true
+      }
+    });
+  
+    chart.render();
+  }
+  
+  function petAnimalsDrilldownHandler(e) {
+    chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      axisY: {
+        title: "Count"
+      },
+      axisX: {
+        title: "Occupation"
+      },
+      data: data[e.dataPoint.drilldown],
+      toolTip: {
+        shared: true
+      }
+    });
+  
+    chart.render();
+  }
+  
